c74792bddc92a96324deec4e04efb61604a1d874
vucic <vucic94@yahoo.com>
2024-08-23 18:43:37 +0200

    Added important feature on test branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello good people!");
+        System.out.println("Doing stuff on test branch");
     }
 }
